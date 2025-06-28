5eeff9754deb4628a51bce1b8ce8902a000aebb7
Xavier13554522 <Xavierggomey@gmail.com>
2025-06-28 15:48:38 -0400

    Update_demo_0.2
    Se coloco un pequeño soporte para celulares,y algunas mejoras

--- scripts.js
+++ scripts.js
@@ -1,6 +1,12 @@
+//Todas las importaciones de los elemento de html
+/**
+ * la variable que se llama draw_timing es la pocion,
+ * y realmente fue un descarte de idea,
+ * ya que iba a ser el parry
+ */
 let draw_timing = document.getElementById("heal");
 const enemy = document.getElementById("fight");
-const game_container = document.getElementById("game_container");
+const game_container = document.getElementById("game-container");
 const one_screen = document.getElementById("button");
 const instructions = document.getElementById("Instructions");
 const list_player = document.getElementById("list_player");
@@ -32,6 +38,7 @@ fight.volume = 0.5;
 kill.preload = "auto";
 curar.preload = "auto";
 skill_sound.preload = "auto";
+//Eventos(todos son el mismo evento XD)
 one_screen.addEventListener("click", firtsStart);
 abyss.addEventListener("click",personajes);
 nameless.addEventListener("click",personajes);
@@ -42,19 +49,22 @@ button_start.addEventListener("click",start);
 skill.addEventListener("click",Player_skill)
 reset.addEventListener("click",recargar);
 final_reset.addEventListener("click",recargar);
+
 let count = 0;
 let pj = ["src/sprite/Hermana_friede/Friede.webp","src/sprite/Gael/Gael.webp","src/sprite/Rey_Sin_nombre/King.webp"];
+//Estadisticas del  jugador y sus personajes,y estadisticas del enemigo
 let player = {
 personaje : ["Hermana Friede","Gael","Rey Sin Nombre"],
 vida : 10,
 ataque : 2,
 }
 let enemigo = {
-vida : 40,
+vida : 50,
 ataque : 2,
 }
 let potions = 5;
 let potions_count = 0;
+//Sirve para dar la pantalla final,para saber si ganaste o perdiste
 function last (){
     display.style.display = "none";
     if(enemigo.vida <= 0){
@@ -65,10 +75,11 @@ function last (){
     }
     final.style.display = "grid";
 }
+//Precarga XDD
 function recargar(){
     location.reload(false);
 }
-//Funciones para que el jugador ataque o se cure y la ia del enemigo
+//Funcion para que el jugador ataque,cure o use su habilidad
 function Accion(){
     enemigo_hp();
     kill.play(1000);
@@ -84,6 +95,31 @@ function Accion(){
         enemigoAtaca();
     }
 }
+function healing(){
+    curar.play(2000);
+    setTimeout(()=>{
+        curar.pause();
+        curar.currentTime = 0;
+    },2000)
+    player.vida += potions;
+    potions_count++;
+    enemigoAtaca();
+}
+function Player_skill(){
+    enemigo_hp();
+    skill_sound.play(3000);
+    setTimeout(()=>{
+        skill_sound.pause();
+        skill_sound.currentTime = 0;
+    },3000)
+    if(enemigo.vida > 0){
+        enemigo.vida -= player.ataque * 4;
+        count = 0;
+        enemigoAtaca();
+    }
+    hp_villain.innerHTML= "<h5>"+"HP:"+ enemigo.vida +"</h5>"
+}
+//Ia del enemigo(el rng es una mierda JAKJAKJ)
 function enemigoAtaca() {
     player_hp();
     let XD = random_start(1,6);
@@ -106,16 +142,7 @@ function enemigoAtaca() {
     btn.style.display = "flex";   
     player_turn();
 }
-function healing(){
-    curar.play(2000);
-    setTimeout(()=>{
-    curar.pause();
-    curar.currentTime = 0;
-    },2000)
-    player.vida += potions;
-    potions_count++;
-    enemigoAtaca();
-}
+//Funcion para la animacion de la vida del enemigo y del jugador
 function player_hp(){
     hp_player.classList.add("player_damage");
     hp_player.style.opacity = "100%";
@@ -132,26 +159,13 @@ function enemigo_hp(){
     hp_villain.innerHTML = "<h5>"+"HP:"+ enemigo.vida +"</h5>"
     },1000)
 }
-function Player_skill(){
-enemigo_hp();
-skill_sound.play(3000);
-setTimeout(()=>{
-skill_sound.pause();
-skill_sound.currentTime = 0;
-},3000)
-if(enemigo.vida > 0){
-    enemigo.vida -= player.ataque * 4;
-    count = 0;
-    enemigoAtaca();
-}
-    hp_villain.innerHTML= "<h5>"+"HP:"+ enemigo.vida +"</h5>"
-}
 //funcion que devuelve un numero random segun el min y el max que se aplique
 function random_start(min,max) {
     min = Math.ceil(min);
     max = Math.floor(max)
     return Math.floor(Math.random()* (max-min)+min)
 }
+//inicio aleatorio,puede empezar el turno del enemigo o el jugador
 function startGame() {
     if(random_start(1,3) === 1){
         player_turn();
@@ -180,6 +194,7 @@ function firtsStart() {
     one_screen.style.display = "none";
     instructions.style.display = "none";
     list_player.style.display = "flex";
+    game_container.style.display = "none";
 }
 function personajes(){
     list_player.style.display = "none";
